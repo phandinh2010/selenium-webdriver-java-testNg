30cff231a6423cc39de3509804d72a91aef9eed5
phandinh2010 <phandinh2010@gmail.com>
2020-12-01 00:42:31 +0700

    Login testcase

--- webdriver/selenium/Topic_01_CheckEnvironment.java
+++ webdriver/selenium/Topic_01_CheckEnvironment.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -18,27 +19,39 @@ public class Topic_01_CheckEnvironment {
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.get("https://translate.google.com/?hl=en&tab=TT&authuser=0");
+		driver.get("http://live.demoguru99.com/");
+		driver.findElements(By.xpath("//span[@class='label']"));
+		driver.findElement(By.xpath("//a[@title='My Account']"));
 	}
 
 	@Test
-	public void TC_01_ValidateCurrentUrl() {
-		// Login Page Url matching
-		String loginPageUrl = driver.getCurrentUrl();
-		Assert.assertEquals(loginPageUrl, "http://demo.guru99.com/v4/");
+	public void TC_01_with_Empty_Username_Password() {
+		driver.findElement(By.xpath("//button[@xpath='1']")).click();
+		//String m = driver.getCurrentUrl();
+		//Assert.assertEquals(loginPageUrl, "http://demo.guru99.com/v4/");
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='advice-required-entry-email']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='advice-required-entry-pass']")).isDisplayed());
 	}
 
 	@Test
-	public void TC_02_ValidatePageTitle() {
+	public void TC_02_with_Invalid_Email() {
 		// Login Page title
-		String loginPageTitle = driver.getTitle();
-		Assert.assertEquals(loginPageTitle, "Guru99 Bank Home Page");
+		//String loginPageTitle = driver.getTitle();
+		//Assert.assertEquals(loginPageTitle, "Guru99 Bank Home Page");
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("123@123");
+		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("12346");
+		driver.findElement(By.xpath("//button[@xpath='1']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//div[text()='Please enter a valid email address. For example johndoe@domain.com.']")).isDisplayed());
 	}
 
 	@Test
-	public void TC_03_LoginFormDisplayed() {
+	public void TC_03_withPassInvalid() {
 		// Login form displayed
-		Assert.assertTrue(driver.findElement(By.xpath("//form[@name='frmLogin']")).isDisplayed());
+		//Assert.assertTrue(driver.findElement(By.xpath("//form[@name='frmLogin']")).isDisplayed());
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("automaiton@gmail.com");
+		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("123");
+		driver.findElement(By.xpath("//button[@xpath='1']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//div[text()='Please enter 6 or more characters without leading or trailing spaces.']")).isDisplayed());
 	}
 
 	@AfterClass
